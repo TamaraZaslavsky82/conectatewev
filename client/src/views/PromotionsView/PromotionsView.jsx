9ca42e45e51220680c00a774014bdd0632981c5f
Juan Cruz Lima Caramico <juanlimacaramico@gmail.com>
2024-12-01 09:32:36 -0300

    adding folder structure

---
+++ client/src/views/PromotionsView/PromotionsView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PromotionsView = () => {
+  return (
+    <div>PromotionsView</div>
+  )
+}
+
+export default PromotionsView
--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import SearchSection from "./SearchSection/SearchSection";
+import MunicipalidadSection from "./MunicipalidadSection/MunicipalidadSection";
+import DestacadosSection from "./DestacadosSection/DestacadosSection";
+import InformationSection from "./InformationSection/InformationSection";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col">
+      <Navbar />
+      <SearchSection/>
+      <MunicipalidadSection/>
+      <DestacadosSection/>
+      <InformationSection/>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
